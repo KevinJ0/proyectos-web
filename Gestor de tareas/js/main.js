4679126a7d7e8997b503b9f32c4fdf3e87177574
kevinj0 <kevinjooo59@gmail.com>
2018-02-01 23:41:03 -0400

    javaScript parte 2

--- Gestor de tareas/js/main.js
+++ Gestor de tareas/js/main.js
@@ -1,30 +1,61 @@
+//variables
+var lista = document.getElementById("lista"),
+  tareaInput = document.getElementById("tareaInput"),
+  btnNuevaTarea = document.getElementById("btn-agregar");
 
+//funciones
+var agregarTarea = function() {
 
-  //variables
-  var lista = document.getElementById("lista"),
-    tareaInput = document.getElementById("tareaInput"),
-    btnNuevaTarea = document.getElementById("btn-agregar");
+  if (comprobarInput()) {
+    var tarea = tareaInput.value,
+      contenido = document.createTextNode(tarea),
+      nuevaTarea = document.createElement("li"),
+      enlace = document.createElement("a");
 
-  //funciones
-  var agregarTarea = function() {
-    alert("si1");
-  };
-  var comprobarInput = function() {
-    alert("si2");
-  };
-  var eliminarTarea = function() {
-    alert("si3");
-  };
+    enlace.appendChild(contenido);
+    enlace.setAttribute("href", "#");
 
-  //Eventos
+    nuevaTarea.appendChild(enlace);
+    lista.append(nuevaTarea);
 
-  //Agregar Tarea
-  btnNuevaTarea.addEventListener("click", agregarTarea);
+    nuevaTarea.addEventListener("click", eliminarTarea);
+    tareaInput.value = "";
 
-  //Comprobar Input
-  tareaInput.addEventListener("click", comprobarInput);
-
-  //Agregar evento borrar
-  for (var i = 0; 0 < lista.children.length-1; i++) {
-    lista.children[i].addEventListener("click", eliminarTarea);
+  } else {
+    showError();
   }
+
+};
+
+var comprobarInput = function() {
+  if (tareaInput.value !== "")
+    return true;
+};
+
+var showError = function() {
+  tareaInput.className = "error";
+  tareaInput.placeholder = "Debe agregar una tarea aca";
+  tareaInput.focus();
+}
+
+var eliminarTarea = function(e) {
+  this.parentNode.removeChild(this);
+};
+
+var cleanErrorInput = function() {
+  tareaInput.className = "";
+  tareaInput.placeholder = "Agrega tu tarea";
+};
+
+//Eventos
+
+//Agregar Tarea
+btnNuevaTarea.addEventListener("click", agregarTarea);
+
+//Comprobar Input
+tareaInput.addEventListener("click", cleanErrorInput);
+
+//Agregar evento borrar
+for (var i = 0; 0 < lista.children.length - 1; i++) {
+  lista.children[i].addEventListener("click", eliminarTarea);
+}
